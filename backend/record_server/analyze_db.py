6cb7704800b8e5ad16f136c5aa1f94598cd223b4
Br1d3e <sub2bridge@outlook.com>
2026-04-22 21:37:00 +0800

    finish database analysis output

--- backend/record_server/analyze_db.py
+++ backend/record_server/analyze_db.py
@@ -1,5 +1,5 @@
 
-from store_db import DATABASE_URL
+from store_db import DATABASE_URL, merkle_tree_root
 import pandas as pd
 import psycopg
 
@@ -8,10 +8,11 @@ class AnalyzeDB:
         self.d_id = None
         self.record = {
             "v": 3,
-            "m": {},
-            "s": []
+            "m": None,
+            "s": None
         }
         self.doc = None
+        self.sessions = None
 
     def _connect(self):
         return psycopg.connect(DATABASE_URL)
@@ -71,6 +72,14 @@ class AnalyzeDB:
             raise ValueError(f"expected exactly one document row for {d_id}, got {len(matched.index)}")
         self.doc = matched.iloc[0]
         return self.doc
+    
+    def get_sessions(self, d_id: str):
+        sessions = self.load_sessions()
+        matched = sessions[sessions["d_id"] == d_id]
+        if matched.empty:
+            raise ValueError(f"sessions not found for {d_id}")
+        self.sessions = matched
+        return self.sessions
         
     def query_title(self, title: str) -> str:
         """
@@ -85,7 +94,7 @@ class AnalyzeDB:
         """
         pass
 
-    def load_d_id(self, d_id: str | None = None, title: str | None = None, author: str | None = None):
+    def load_doc(self, d_id: str | None = None, title: str | None = None, author: str | None = None):
         if self.d_id:
             return self.d_id
         if d_id:
@@ -96,6 +105,10 @@ class AnalyzeDB:
             self.d_id = self.query_author(author)
         else:
             return None
+        self.record["m"] = None
+        self.record["s"] = None
+        self.get_doc(self.d_id)
+        self.get_sessions(self.d_id)
         return self.d_id
 
     def set_d_id(self, d_id: str):
@@ -103,30 +116,107 @@ class AnalyzeDB:
 
     def get_d_id(self):
         return self.d_id
-    
 
     def fetch_doc_meta(self):
-        if not self.d_id:
+        if not isinstance(self.doc, pd.Series):
             return
-        self.get_doc(self.d_id)
-        self.record["m"] = {
+        m = {
             "dId": self.doc["d_id"],
-            "t0": self.doc["t0"],
-            "tn": self.doc["updated_server_ts"],
+            "t0": int(self.doc["t0"]),
+            "tn": int(self.doc["updated_server_ts"]),
             "ttl": self.doc["title"],
             "a": self.doc["author"]
         }
+        self.record["m"] = m
+        return m
 
+    def fetch_blocks(self, sid: str):
+        blocks = self.load_blocks(sid, d_id=self.d_id)
+        blocks_parsed = blocks[[
+            "q",
+            "ch",
+            "dt0",
+            "dtn",
+            "ev",
+            "received_server_ts",
+            "valid_q",
+            "valid_h",
+            "valid_dsh",
+            "valid_n",
+            "freshness_status",
+        ]].copy()
+        
+        blocks_parsed["status"] = self.get_blocks_status(blocks_parsed)
+        blocks_parsed = blocks_parsed.drop(
+            columns=["valid_q", "valid_h", "valid_dsh", "valid_n", "freshness_status"]
+        )
+        return blocks_parsed
+
+    def get_blocks_status(self, blocks_parsed: pd.DataFrame):
+        if blocks_parsed.empty:
+            return None
+        blocks_parsed = blocks_parsed.copy().sort_values(by=["q"])
+        blocks_status = []
+        for i in range(len(blocks_parsed)):
+            block = blocks_parsed.iloc[i]
+            
+            valid_q = block["valid_q"]
+            valid_h = block["valid_h"]
+            valid_dsh = block["valid_dsh"]
+            valid_n = block["valid_n"]
+            freshness_status = block["freshness_status"]
+            block_status = []
+            if valid_q and valid_h and valid_dsh and valid_n and freshness_status == "FRESH":
+                block_status.append("VALID")
+            if not valid_q:
+                block_status.append("INVALID_Q")
+            if not valid_h or not valid_dsh:
+                block_status.append("INVALID_HASH")
+            if not valid_n or freshness_status != "FRESH":
+                block_status.append("INVALID_FRESHNESS")
+        
+            blocks_status.append(block_status)
+
+        return blocks_status
+
+    def fetch_sessions(self):
+        if not isinstance(self.sessions, pd.DataFrame) or not self.d_id:
+            return
+        s = []
+        for i in range(len(self.sessions)):
+            session = self.sessions.iloc[i]
+            sid = session["sid"]
+            blocks = self.fetch_blocks(sid)
+
+            if blocks.empty:
+                continue
+            
+            mapping = {"TRUE": True, "FALSE": False, "UNKNOWN": None}
+        
+            s.append({
+                "sid": sid,
+                "t0": int(session["st0"]),
+                "tn": int(session["st0"] + session["dt"]),
+                "it": session["init_text"],
+                # "ev": session["ev"],
+                "b": blocks.drop(columns=["ch"]).to_dict(orient="records"),
+                "cs": mapping.get(session["continuity_status"]),
+                "mr": session["merkle_root"] == merkle_tree_root(blocks["ch"].to_list())
+            })
+        self.record["s"] = s
+        return s            
 
     def get_record(self):
         if not self.d_id:
             raise ValueError("d_id is not loaded")
         if self.record["m"] is None:
             self.fetch_doc_meta()
+        if self.record["s"] is None:
+            self.fetch_sessions()
         return self.record
 
 
 if __name__ == "__main__":
     a = AnalyzeDB()
-    a.load_d_id(d_id="83cd8058-ee7d-4097-b7fa-d11c543c98c5")
+    a.load_doc(d_id="83cd8058-ee7d-4097-b7fa-d11c543c98c5")
     print(a.get_record())
