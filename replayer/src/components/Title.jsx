8e75ae1daebb3a39cb7686952906523fa0ec2556
Br1d3e <sub2bridge@outlook.com>
2026-04-29 09:43:18 +0800

    replayer search database query + App record state

---
+++ replayer/src/components/Title.jsx
@@ -0,0 +1,7 @@
+export default function Title({ title = "VeriWrite Record Replayer" }) {
+  return (
+    <>
+      <h1 className="text-3xl font-bold justify-self-center mt-20">{title}</h1>
+    </>
+  );
+}
